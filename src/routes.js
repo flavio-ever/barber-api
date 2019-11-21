9677472279e8aaa6bcbf4fc7cbf90fcfd23fb61a
Flavio Ever <flavio.ever@icloud.com>
2019-11-21 16:47:19 -0300

    Appointments, Notification and  Mail

--- src/routes.js
+++ src/routes.js
@@ -9,19 +9,29 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', AuthMiddleware, UserController.store);
-routes.put('/users', AuthMiddleware, UserController.update);
-
-routes.post('/appointments', AuthMiddleware, AppointmentController.store);
-routes.get('/appointments', AuthMiddleware, AppointmentController.index);
-
 routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(AuthMiddleware);
+
+routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/shedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post(
   '/files',
