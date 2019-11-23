6d989553e8bc8c2b7318a1daaaf0476aabab2533
Flavio Ever <flavio.ever@icloud.com>
2019-11-23 20:07:30 -0300

    Sentry + Express async

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
-import path from 'path';
-import routes from './routes';
 import './database';
+import 'express-async-errors';
+import * as Sentry from '@sentry/node';
+import path from 'path';
+import Youch from 'youch';
+import sentryConfig from './config/sentry';
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.exceptionHanddler();
   }
 
   middlewares() {
+    // Log de erros (antes de qualquer evento)
+    Sentry.init(sentryConfig);
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +29,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHanddler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 export default new App().server;
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://6750e450569b42fb8534ae8ec06ef86b@sentry.io/1832175',
+};
