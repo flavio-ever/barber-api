108c280fd6f291706fd4984222f17f9977836bba
Flavio Ever <flavio.ever@icloud.com>
2019-11-23 18:19:59 -0300

    Appointment Scheduling and Error Logging in Queue

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -6,6 +7,20 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            // Se data de agendamento é anterior a data atual
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            // Se data atual é anterior a data de agendamento - 2hrs
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
